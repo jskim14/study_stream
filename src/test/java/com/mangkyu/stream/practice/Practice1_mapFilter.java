147dc43c4def423ca8c1399de2849927c711c0ad
jskim <js.story14@gmail.com>
2023-10-26 13:34:42 +0900

    연습추가) 객체리스트, flatMap

--- src/test/java/com/mangkyu/stream/practice/Practice1_mapFilter.java
+++ src/test/java/com/mangkyu/stream/practice/Practice1_mapFilter.java
@@ -1,13 +1,17 @@
 package com.mangkyu.stream.practice;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.nio.file.FileAlreadyExistsException;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Practice1_mapFilter {
 
@@ -15,6 +19,7 @@ public class Practice1_mapFilter {
     Map<String,String> map2;
     List<Map<String,String>> mapList;
     List<String> stringList;
+    List<Person> personList;
 
     @BeforeEach
     void setUp() {
@@ -22,6 +27,7 @@ public class Practice1_mapFilter {
         map2 = Map.of("키움","승리를 위한 함~성","두산","걸어간다 양의지","기아","기아의 김~주찬");
         mapList = List.of(map,map2);
         stringList = List.of("Kiwoom","Hanhwa","Doosan", "heroes");
+        personList = List.of(new Person("박병호","30"), new Person("최원태","20"), new Person("김상수", "30"));
     }
 
     @Test
@@ -69,5 +75,49 @@ public class Practice1_mapFilter {
         System.out.println("strings1 = " + strings1); //strings1 = [Hanhwa, Doosan]
 
         System.out.println("End");
+    }
+
+    @Test
+    void practice3() {
+        /*
+        * 객체
+        * */
+        personList.stream()
+                .map(s -> s.name)
+                .collect(Collectors.toList())
+                .forEach(s -> System.out.print(s + " ")); // 박병호 최원태 김상수
+
+        personList.stream()
+                .filter(s -> s.age.equals("30"))
+                .map(s -> s.name)
+                .collect(Collectors.toList());
+
+        System.out.println("END");
+    }
+
+    @Test
+    void practice4() {
+        List<String[]> collect = stringList.stream()
+                .map(s -> s.split(""))
+                .collect(Collectors.toList());
+
+        List<Map.Entry<String, String>> collect1 = mapList.stream()
+                .flatMap(s -> s.entrySet().stream())
+                .filter(s -> s.getKey().equals("키움"))
+                .collect(Collectors.toList());
+
+        System.out.println("collect = " + collect1);
+    }
+
+    @Getter
+    @Setter
+    private static class Person {
+        private String name;
+        private String age;
+
+        public Person(String name, String age) {
+            this.name = name;
+            this.age = age;
+        }
     }
 }
