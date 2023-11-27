ca0c090dd682263707f1b27b36584383e16a6ffc
jskim <js.story14@gmail.com>
2023-11-27 19:00:07 +0900

    quiz3.1 모든 숫자 쌍의 배열리스트

--- src/main/java/com/mangkyu/stream/Quiz3/Quiz3.java
+++ src/main/java/com/mangkyu/stream/Quiz3/Quiz3.java
@@ -15,9 +15,11 @@ public class Quiz3 {
     // ex) numbers1 = [1,2,3], numbers2 = [3,4] -> [(1,3), (1,4), (2,3), (2,4), (3,3), (3,4)]
     public List<Integer[]> quiz1() {
 
-        return numbers1.stream()
+        List<Integer[]> collect = numbers1.stream()
                 .flatMap(num1 -> (numbers2.stream().map(num2 -> new Integer[]{num1, num2}))
                 ).collect(Collectors.toList());
+
+        return collect;
     }
 
     // 3.2 모든 숫자 쌍의 곱이 가장 큰 값을 반환하여라.
